6928bfb325b8a36efae6de92df7a2f1a4e8d30f9
rockerle <16375280+rockerle@users.noreply.github.com>
2025-04-11 17:40:11 +0200

    mhmm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
+echo "$INPUT_IMAGENAME"
+echo "$INPUT_CONTAINERNAME"
+echo "$INPUT_RESTARTPOLICY"
+
 printenv > .env
 docker stop "$INPUT_CONTAINERNAME" || echo "No container running with that name"
 docker rm "$INPUT_CONTAINERNAME" || echo "No container found to remove"
